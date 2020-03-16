cb55f96e703d007138c25be703c5f6327811a20d
Jota777 <62240175+Jota777@users.noreply.github.com>
2020-03-16 11:47:02 +0100

    recon-all_long.sh
    original

---
+++ recon-all_long.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+#set variables
+patid=$1
+STUDY_DIR=/media/Daten1/Rasmussen
+
+
+echo "--- recon-all longitudinal start subject ${patid} at $(date) --- "
+cd ${STUDY_DIR}/${patid}
+pwd
+
+#create timepoint list, wählt er hier wirklich nur die tps aus, siehe Foto auf wa?
+list_tps=$(ls | grep -E "_[0-9]+")
+
+for tps in ${list_tps}
+do
+	ln -s ${STUDY_DIR}/${patid}/${tps}/freesurfer_${tps} freesurfer_${tps}
+	echo -n "-tp freesurfer_${tps} " >> tplist_${patid}.txt
+
+done
+
+#create recon-all base
+
+echo "start recon-all base for ${patid}"
+recon-all -base ${patid}_base $(cat tplist_${patid}.txt) -all -sd ./
+echo "finished recon-all base for ${patid}"
+
+#create longitunials according to base
+
+for tps in ${list_tps}
+do
+	echo "start recon-all -long for ${tps}"  
+	recon-all -long freesurfer_${tps} ${patid}_base -all -sd ./
+	echo "finished recon-all -long for ${tps}"
+done
+
+echo "--- recon-all longitudinal finished subject ${patid} at $(date) --- "
+
